3a1d2c018bbb1934a1275dc34b83d373cb98dff5
bysju <bysju@hotmail.com>
2017-09-07 12:50:18 +0900

    add test case for list

--- test_bencodeserialzation/testbencodeserialzation.cpp
+++ test_bencodeserialzation/testbencodeserialzation.cpp
@@ -10,6 +10,34 @@
 
 // bencode specification https://wiki.theory.org/index.php/BitTorrentSpecification
 
+// Test Function
+
+void TestInterger(int value, std::shared_ptr<bencode::BencodeType> bType)
+{
+	REQUIRE(bencode::BENCODE_TYPE::INTEGER == bType->GetType());
+
+	bencode::BencodeInteger * pBencodeInteger = dynamic_cast<bencode::BencodeInteger*>(bType.get());
+
+
+	INFO("Inteager is: " << value << " Bencode Type is: " << pBencodeInteger->GetValue());
+	REQUIRE(pBencodeInteger->GetValue() == value);
+
+}
+
+void TestInteger(const std::string & strValue, std::shared_ptr<bencode::BencodeType> bType)
+{
+	REQUIRE(bencode::BENCODE_TYPE::INTEGER == bType->GetType());
+
+	bencode::BencodeInteger * pBencodeInteger = dynamic_cast<bencode::BencodeInteger*>(bType.get());
+
+	std::stringstream oss;
+	bencode::BencodeOArchive bencodeOArchive(oss);
+
+
+//	INFO("Inteager sring is: " << strValue << " Bencode Type is: " << pBencodeInteger->GetValue());
+//	REQUIRE(pBencodeInteger->GetValue() == strValue);
+}
+
 SCENARIO("Bencode Interger Test/Integer to Bencode")
 {
 	// -99 -> i-99e
@@ -20,7 +48,7 @@ SCENARIO("Bencode Interger Test/Integer to Bencode")
 		std::stringstream oss;
 		bencode::BencodeOArchive bencodeOArchive(oss);
 
-		std::unique_ptr<bencode::BencodeType> bInteger(new bencode::BencodeInteger(i));
+		std::shared_ptr<bencode::BencodeType> bInteger(new bencode::BencodeInteger(i));
 		bencodeOArchive << bInteger.get();
 
 		// make correct result
@@ -49,7 +77,7 @@ SCENARIO("Bencode Interger Test/Bencode to Integer")
 		bencodeIArchive >> pBencodeType;
 
 		REQUIRE(pBencodeType->GetType() == bencode::BENCODE_TYPE::INTEGER);
-		std::unique_ptr<bencode::BencodeType> bInteger(pBencodeType);
+		std::shared_ptr<bencode::BencodeType> bInteger(pBencodeType);
 		bencode::BencodeInteger * pBencodeInteger = dynamic_cast<bencode::BencodeInteger*>(pBencodeType);
 
 
@@ -77,7 +105,7 @@ SCENARIO("Bencode String Test/String to Bencode")
 		std::stringstream correctVal;
 		correctVal << str.size() << ":" << str;
 
-		std::unique_ptr<bencode::BencodeType> bString(new bencode::BencodeString(str));
+		std::shared_ptr<bencode::BencodeType> bString(new bencode::BencodeString(str));
 		bencodeOArchive << bString.get();
 
 		INFO("String is: " << str << " String is: " << correctVal.str());
@@ -115,4 +143,67 @@ SCENARIO("Bencode String Test/Bencode to String")
 		INFO("String is: " << pBencodeString->GetValue() << " String is: " << iss.str());
 		REQUIRE(pBencodeString->GetValue() == str);
 	}
+}
+
+SCENARIO("Bencode List Test/List to Bencode")
+{
+	
+		
+	std::shared_ptr<bencode::BencodeType> bStrSpam(new bencode::BencodeString("spam"));
+	std::shared_ptr<bencode::BencodeType> bStrEggs(new bencode::BencodeString("eggs"));
+	std::shared_ptr<bencode::BencodeType> bInt9(new bencode::BencodeInteger(9));
+
+
+	SECTION("string List")
+	{
+		// l4:spam4:eggse represents the list of two strings: [ "spam", "eggs" ] 
+		std::string strTestResult = "l4:spam4:eggse";
+
+		std::unique_ptr<bencode::BencodeList> bListTest(new bencode::BencodeList());
+		bListTest->push_back(bStrSpam);
+		bListTest->push_back(bStrEggs);
+
+		std::stringstream oss;
+		bencode::BencodeOArchive bencodeOArchive(oss);
+
+		bencodeOArchive << bListTest.get();
+
+		REQUIRE(oss.str() == strTestResult);
+
+
+	}
+	
+	SECTION("empty List")
+	{
+		// Example: le represents an empty list : []
+		std::string strTestResult = "le";
+
+		std::unique_ptr<bencode::BencodeList> bListTest(new bencode::BencodeList());
+
+		std::stringstream oss;
+		bencode::BencodeOArchive bencodeOArchive(oss);
+
+		bencodeOArchive << bListTest.get();
+
+		REQUIRE(oss.str() == strTestResult);
+	}
+
+	SECTION("Strng/Integer List")
+	{
+		// l4:spam4:i9ee represents the list of two strings: [ "spam", 9 ] 
+		std::string strTestResult = "l4:spami9ee";
+
+		std::unique_ptr<bencode::BencodeList> bListTest(new bencode::BencodeList());
+		bListTest->push_back(bStrSpam);
+		bListTest->push_back(bInt9);
+
+		std::stringstream oss;
+		bencode::BencodeOArchive bencodeOArchive(oss);
+
+		bencodeOArchive << bListTest.get();
+
+		REQUIRE(oss.str() == strTestResult);
+	}
+	
+
 }
